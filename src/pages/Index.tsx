da6c78f25005609ac8ad3d208e9d72b6dc4a79dd
Abhay Ramagiri <abhayramagiri124@gmail.com>
2025-12-22 02:45:10 +0530

    Initial full project commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,31 +1,24 @@
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
-import SkillsSection from '@/components/SkillsSection';
-import ExperienceSection from '@/components/ExperienceSection';
-import WorkShowcase from '@/components/WorkShowcase';
 import ProjectsSection from '@/components/ProjectsSection';
-import BeyondWork from '@/components/BeyondWork';
+import WorkShowcase from '@/components/WorkShowcase';
 import ContactSection from '@/components/ContactSection';
-import StrategyToExecutionBridge from '@/components/StrategyToExecutionBridge';
-
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
+    <>
       <Header />
+
+      {/* ❌ NO padding, NO margin here */}
       <main>
         <HeroSection />
         <AboutSection />
-        <SkillsSection />
-        <ExperienceSection />
         <ProjectsSection />
-        <StrategyToExecutionBridge />
         <WorkShowcase />
-        <BeyondWork />
         <ContactSection />
       </main>
-    </div>
+    </>
   );
 };
 
